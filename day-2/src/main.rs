d487808fd86c38f585c9b1819a2e473d4506a88a
zP0 <zP@solarsystem.coffee>
2023-12-06 10:05:43 -0900

    day-2b complete

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -9,13 +9,11 @@ fn main() {
         b: 14,
     };
     let data: Vec<String> = read_lines();
-    /*for line in data {
-        let a = extract_cubes(line);
-        println!("{}", a);
-    }*/
-    let not_positionz: Vec<i32> = possible_ID(data);
-    let possible: Vec<i32> = get_vec_possible(not_positionz);
-    let sum: i32 = pos_add(possible);
+    //let not_positionz: Vec<i32> = possible_ID(data);
+    //let possible: Vec<i32> = get_vec_possible(not_positionz);
+    //let sum: i32 = pos_add(possible);
+    let power_c: Vec<i32> = power(data);
+    let sum: i32 = pos_add(power_c);
     println!("sum: {}", sum.to_string());
 }
 
@@ -56,6 +54,49 @@ fn duplicatez_remove(data: Vec<i32>) -> Vec<i32> {
     unique_numb.into_iter().collect()
 }
 
+fn power(data: Vec<String>) -> Vec<i32> {
+    let game_ID_re = Regex::new(r"Game (\d+):").unwrap();
+    let colorz_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
+    let mut power_a: Vec<i32> = Vec::new();
+    for line in data {
+        let mut red: i32 = 1;
+        let mut green: i32 = 1;
+        let mut blue: i32 = 1;
+        println!("{}", line);
+        if let Some(game_id_cap) = game_ID_re.captures(&line) {
+            let _game_id: i32 = game_id_cap[1]
+                .parse::<i32>()
+                .expect("couldnt parse game_ID");
+            let color_set_str = &line[game_id_cap[0].len()..];
+            let color_set = color_set_str.split(";").filter(|s| !s.is_empty());
+            for set in color_set {
+                //println!("set = {}", set.to_string());
+                for color_c in colorz_re.captures_iter(set) {
+                    let value = color_c[1].parse::<i32>().unwrap();
+                    let color = color_c[2].to_string();
+                    //println!(
+                    //    "color = {} value = {}",
+                    //    color.to_string(),
+                    //    value.to_string()
+                    //);
+                    if color == "red".to_string() && value > red {
+                        red = value;
+                    }
+                    if color == "green".to_string() && value > green {
+                        green = value;
+                    }
+                    if color == "blue".to_string() && value > blue {
+                        blue = value;
+                    }
+                }
+            }
+            let power: i32 = red * green * blue;
+            println!("power {}", power.to_string());
+            power_a.push(power);
+        }
+    }
+    power_a
+}
 fn possible_ID(data: Vec<String>) -> Vec<i32> {
     let game_ID_re = Regex::new(r"Game (\d+):").unwrap();
     let colorz_re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
